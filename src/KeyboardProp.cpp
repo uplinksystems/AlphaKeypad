0055ba989c0765a64c212d3d41a2591e2198bebd
LogicMaster63 <justinmarentette11@gmail.com>
2019-07-24 18:15:02 -0400

    Initial Commit

---
+++ src/KeyboardProp.cpp
@@ -0,0 +1,151 @@
+#include <hidboot.h>
+#include <usbhub.h>
+#include <Arduino.h>
+
+// Satisfy the IDE, which needs to see the include statment in the ino too.
+#ifdef dobogusinclude
+#include <spi4teensy3.h>
+#endif
+#include <SPI.h>
+
+const char code[] = "password";
+char chars[] = "12345678";
+
+void PrintString(const char *str) {
+    const char *p;
+    p = str;
+    while (*p) {
+        Serial.print(*p);
+        p++;
+    }
+    Serial.println();
+}
+
+class KbdRptParser : public KeyboardReportParser
+{
+    void PrintKey(uint8_t mod, uint8_t key);
+
+protected:
+    void OnControlKeysChanged(uint8_t before, uint8_t after);
+
+    void OnKeyDown	(uint8_t mod, uint8_t key);
+    void OnKeyUp	(uint8_t mod, uint8_t key);
+    void OnKeyPressed(uint8_t key);
+};
+
+void KbdRptParser::PrintKey(uint8_t m, uint8_t key)
+{
+    MODIFIERKEYS mod;
+    *((uint8_t*)&mod) = m;
+    Serial.print((mod.bmLeftCtrl   == 1) ? "C" : " ");
+    Serial.print((mod.bmLeftShift  == 1) ? "S" : " ");
+    Serial.print((mod.bmLeftAlt    == 1) ? "A" : " ");
+    Serial.print((mod.bmLeftGUI    == 1) ? "G" : " ");
+
+    Serial.print(" >");
+    PrintHex<uint8_t>(key, 0x80);
+    Serial.print("< ");
+
+    Serial.print((mod.bmRightCtrl   == 1) ? "C" : " ");
+    Serial.print((mod.bmRightShift  == 1) ? "S" : " ");
+    Serial.print((mod.bmRightAlt    == 1) ? "A" : " ");
+    Serial.println((mod.bmRightGUI    == 1) ? "G" : " ");
+};
+
+void KbdRptParser::OnKeyDown(uint8_t mod, uint8_t key)
+{
+    for (int i = 1; i < sizeof(code) - 1; i++) {
+            chars[i - 1] = chars[i];
+    }
+    chars[sizeof(code) - 2] = (char)OemToAscii(mod, key);
+    Serial.print("chars: ");
+    PrintString(chars);
+    Serial.print("code: ");
+    PrintString(code);
+    if (strcmp(code, chars) == 0) {
+        Serial.println("Correct password has been entered");
+        digitalWrite(2, true);
+    }
+    PrintKey(mod, key);
+    uint8_t c = OemToAscii(mod, key);
+
+    if (c)
+        OnKeyPressed(c);
+}
+
+void KbdRptParser::OnControlKeysChanged(uint8_t before, uint8_t after) {
+
+    MODIFIERKEYS beforeMod;
+    *((uint8_t*)&beforeMod) = before;
+
+    MODIFIERKEYS afterMod;
+    *((uint8_t*)&afterMod) = after;
+
+    if (beforeMod.bmLeftCtrl != afterMod.bmLeftCtrl) {
+        Serial.println("LeftCtrl changed");
+    }
+    if (beforeMod.bmLeftShift != afterMod.bmLeftShift) {
+        Serial.println("LeftShift changed");
+    }
+    if (beforeMod.bmLeftAlt != afterMod.bmLeftAlt) {
+        Serial.println("LeftAlt changed");
+    }
+    if (beforeMod.bmLeftGUI != afterMod.bmLeftGUI) {
+        Serial.println("LeftGUI changed");
+    }
+
+    if (beforeMod.bmRightCtrl != afterMod.bmRightCtrl) {
+        Serial.println("RightCtrl changed");
+    }
+    if (beforeMod.bmRightShift != afterMod.bmRightShift) {
+        Serial.println("RightShift changed");
+    }
+    if (beforeMod.bmRightAlt != afterMod.bmRightAlt) {
+        Serial.println("RightAlt changed");
+    }
+    if (beforeMod.bmRightGUI != afterMod.bmRightGUI) {
+        Serial.println("RightGUI changed");
+    }
+
+}
+
+void KbdRptParser::OnKeyUp(uint8_t mod, uint8_t key)
+{
+    Serial.print("UP ");
+    PrintKey(mod, key);
+}
+
+void KbdRptParser::OnKeyPressed(uint8_t key)
+{
+    Serial.print("ASCII: ");
+    Serial.println((char)key);
+};
+
+USB     Usb;
+//USBHub     Hub(&Usb);
+HIDBoot<USB_HID_PROTOCOL_KEYBOARD>    HidKeyboard(&Usb);
+
+KbdRptParser Prs;
+
+void setup()
+{
+    pinMode(2, OUTPUT);
+    digitalWrite(2, false);
+    Serial.begin( 115200 );
+#if !defined(__MIPSEL__)
+    while (!Serial); // Wait for serial port to connect - used on Leonardo, Teensy and other boards with built-in USB CDC serial connection
+#endif
+    Serial.println("Start");
+
+    if (Usb.Init() == -1)
+        Serial.println("OSC did not start.");
+
+    delay( 200 );
+
+    HidKeyboard.SetReportParser(0, &Prs);
+}
+
+void loop()
+{
+    Usb.Task();
+}
